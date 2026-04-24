2574181b1a9704b2e5ebf002bf7c6e0df21371ae
Ko <masmask0506@gmail.com>
2026-04-25 02:40:42 +0900

    localStorage追加

--- app.js
+++ app.js
@@ -9,9 +9,41 @@ function addTask() {
 
   li.onclick = function () {
     li.remove();
+    saveTasks();
   };
 
   document.getElementById("taskList").appendChild(li);
 
   input.value = "";
+
+  saveTasks();
 }
+
+// 保存
+function saveTasks() {
+  const tasks = [];
+  document.querySelectorAll("#taskList li").forEach(li => {
+    tasks.push(li.textContent);
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// 読み込み
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  tasks.forEach(task => {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    li.onclick = function () {
+      li.remove();
+      saveTasks();
+    };
+
+    document.getElementById("taskList").appendChild(li);
+  });
+}
+
+loadTasks();
